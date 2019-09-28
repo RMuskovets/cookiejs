cc7f55001045240d60546d9329de2f399b3ae946
RMuskovets <rmuskovets@gmail.com>
2019-09-28 11:58:27 +0300

    Create main library for plain browsers.

---
+++ src/cookie.js
@@ -0,0 +1,12 @@
+(function () {
+    let cookies = document.cookie.split("; ").map(x=>x.split("=")).reduce((acc,[x,y]) => {acc[x]=y;return acc},{});
+    const getCookie = (name) => cookies[name];
+    const setCookie = (name, value) => cookies[name] = value;
+    const flushCookies = () => {
+        let cookieString = "";
+        let cookieArray = Object.keys(cookies).map(key=>[key, cookies[key]]);
+        cookieString = cookieArray.map(([k,v])=>`${encodeURIComponent(k)}=${encodeURIComponent(v)}`).join("; ");
+        document.cookie = cookieString;
+    }
+	window.cookie = { getCookie, setCookie, flushCookies };
+})();
